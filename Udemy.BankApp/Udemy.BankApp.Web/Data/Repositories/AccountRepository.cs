8ea79d64f9eaed736d8de3eff59b02d7a8e5f349
Kemal Şen <55662927+kemlsen@users.noreply.github.com>
2022-09-25 21:04:45 +0300

    BankApp

---
+++ Udemy.BankApp/Udemy.BankApp.Web/Data/Repositories/AccountRepository.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Udemy.BankApp.Web.Data.Context;
+using Udemy.BankApp.Web.Data.Entities;
+using Udemy.BankApp.Web.Data.Interfaces;
+
+namespace Udemy.BankApp.Web.Data.Repositories
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private readonly BankContext _context;
+
+        public AccountRepository(BankContext context)
+        {
+            _context = context;
+        }
+
+        public void Create(Account user)
+        {
+            _context.Set<Account>().Add(user);
+            _context.SaveChanges();
+        }
+        public void Remove(Account account)
+        {
+            _context.Set<Account>().Remove(account);
+            _context.SaveChanges();
+        }
+        public List<Account> GetAll()
+        {
+            return _context.Set<Account>().ToList();
+        }
+    }
+}
---
+++ Udemy.BankApp/Udemy.BankApp.Web/TagHelpers/GetAccountCount.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Linq;
+using Udemy.BankApp.Web.Data.Context;
+
+namespace Udemy.BankApp.Web.TagHelpers
+{
+    [HtmlTargetElement("getAccountCount")]
+    public class GetAccountCount : TagHelper
+    {
+        private readonly BankContext _context;
+        public int ApplicationUserId { get; set; }
+        public GetAccountCount(BankContext context)
+        {
+            _context = context;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var accountCount = _context.Accounts.Count(x => x.ApplicationUserId == ApplicationUserId);
+            var html = $"<span class='badge bg-danger'>{accountCount}</span>";
+            output.Content.SetHtmlContent(html);
+        }
+    }
+}
